b02d825d1277a1bfda21d6e48ba048ea21c0cd10
saldatkin <aliaksei.saldatkin@gmail.com>
2022-11-15 23:04:02 +0200

    feat: add different base layers

--- js/index.js
+++ js/index.js
@@ -4,8 +4,10 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic2FsZGF0a2luIiwiYSI6ImNsOXp5aXZkYzBoczkzbm52O
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [25.25, 54.7],
-  zoom: 10
+  center: [25.19, 54.7],
+  zoom: 10,
+  pitch: 65,
+  maxZoom: 15
 });
 
 
@@ -58,22 +60,58 @@ const geojson = {
 
 
 for (const feature of geojson.features) {
-  // create a HTML element for each feature
   const el = document.createElement('div');
+
   el.className = 
-  `marker ${feature
-    .properties
-    .description
-    .toLowerCase()
-    .replace(/\s+/g, '')}`;
+    `marker ${feature
+      .properties
+      .description
+      .toLowerCase()
+      .replace(/\s+/g, '')}`;
+
 
   new mapboxgl.Marker(el)
   .setLngLat(feature.geometry.coordinates)
   .setPopup(
-    new mapboxgl.Popup({ offset: 25 }) // add popups
+    new mapboxgl.Popup({ 
+        offset: 25,
+        closeOnMove: true,
+        closeButton: false,
+        closeOnClick: true 
+      })
       .setHTML(
         `<h3>saldatkin</h3><p>${feature.properties.description}</p>`
       )
   )
   .addTo(map);
 }
+
+map.addControl(new mapboxgl.NavigationControl())
+
+const layerList = document.getElementById('menu');
+const inputs = layerList.getElementsByTagName('input');
+ 
+for (const input of inputs) {
+  input.onclick = (layer) => {
+    const layerId = layer.target.id;
+    map.setStyle('mapbox://styles/mapbox/' + layerId);
+  };
+}
+
+map.on('load', () => {
+  map.addLayer({
+    id: 'points-of-interest',
+    source: {
+      type: 'vector',
+      url: 'mapbox://mapbox.mapbox-streets-v8'
+    },
+    'source-layer': 'poi_label',
+    type: 'circle',
+    paint: {
+      'circle-color': '#223b53',
+    },
+    layout: {
+    // Mapbox Style Specification layout properties
+    }
+    });
+});
